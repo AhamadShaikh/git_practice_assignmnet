361db1844fe9e1863db69c7dd4b8df5406692b57
AhamadShaikh <shaikhahamadrasul786@gmail.com>
2022-12-07 15:35:51 +0530

    prime code is fix

--- Prime.js
+++ Prime.js
@@ -1,11 +1,11 @@
 let num = 13;
 let count = 0;
-for(let i=1; i<num; i++){
+for(let i=1; i<=num; i++){
   	if(num % i == 0){
 		count++;
 	}
 }
-if(count > 2){
+if(count == 2){
 	console.log(num,"is Prime Number");
 }else{
 	console.log(num,"is Not a Prime Number");
